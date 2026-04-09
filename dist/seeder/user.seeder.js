74c46e0f183028fa9dd08ca45d20bb103512067b
Jeppe Vad Andersen <Jeppe.vadandersen@gmail.com>
2026-04-09 15:09:08 +0200

    Update: Realistic Coordinates and TOA datapoints

--- dist/seeder/user.seeder.js
+++ dist/seeder/user.seeder.js
@@ -1,5 +1,20 @@
 import { fakerDA as faker } from '@faker-js/faker';
+// ───────────────────────────────────────────────────────────────
+//  :::::: SEEDING CONTROL PANEL ::::::
+// ───────────────────────────────────────────────────────────────
+// Set the seed for the seeder - same seed will always generate same result.
 faker.seed(69420);
+// Set the centerLocation which will be the destination for all users.
+let centerLocation = {
+    address: "Fibigerstræde 15, 9220 Aalborg",
+    coordinates: [57.0161, 9.97759],
+};
+// Set the coordinate deviation for origin points.
+let d = 0.011192;
+// Set the time interval
+let hourTimes = ["08", "09"];
+let minuteTimes = ["00", "15", "30", "45"];
+// ───────────────────────────────────────────────────────────────
 let idCounter = 0;
 let weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 export function createRandomUser() {
@@ -13,13 +28,23 @@ export function createRandomUser() {
             carpoolingIntent: true,
             pickupPoint: {
                 address: String(faker.location.streetAddress()),
-                coordinates: [Number(faker.location.latitude()), Number(faker.location.latitude())]
+                coordinates: [
+                    Number(faker.location.latitude({
+                        min: centerLocation.coordinates[0] - d,
+                        max: centerLocation.coordinates[0] + d
+                    })),
+                    Number(faker.location.latitude({
+                        min: centerLocation.coordinates[1] - d,
+                        max: centerLocation.coordinates[1] + d
+                    }))
+                ]
             },
-            destination: {
-                address: String(faker.location.streetAddress()),
-                coordinates: [Number(faker.location.latitude()), Number(faker.location.latitude())]
-            },
-            timeOfArrival: String(faker.date.soon()),
+            destination: centerLocation,
+            timeOfArrival: (() => {
+                const hour = faker.number.int({ min: 0, max: hourTimes.length - 1 });
+                const minute = faker.number.int({ min: 0, max: minuteTimes.length - 1 });
+                return `${hourTimes[hour]}:${minuteTimes[minute]}`;
+            })()
         };
         calender.push(calenderDay);
     }
--- src/seeder/user.seeder.ts
+++ src/seeder/user.seeder.ts
@@ -1,12 +1,38 @@
 import { fakerDA as faker } from '@faker-js/faker';
 import type { User } from '../models/user.model.js';
 import type { CalenderDay } from '../models/calender_day.model.js';
+import type { Location } from '../models/location.model.js';
 
+
+// ───────────────────────────────────────────────────────────────
+//  :::::: SEEDING CONTROL PANEL ::::::
+// ───────────────────────────────────────────────────────────────
+
+// Set the seed for the seeder - same seed will always generate same result.
 faker.seed(69420);
+
+// Set the centerLocation which will be the destination for all users.
+let centerLocation: Location = {
+    address: "Fibigerstræde 15, 9220 Aalborg",
+    coordinates: [57.0161, 9.97759],
+}
+
+// Set the coordinate deviation for origin points.
+let d: number = 0.011192;
+
+// Set the time interval
+let hourTimes: string[] = ["08", "09"];
+let minuteTimes: string[] = ["00", "15", "30", "45"];
+
+
+
+// ───────────────────────────────────────────────────────────────
+
 
 
 let idCounter = 0;
 let weekDays: [string, string, string, string, string] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+
 
 export function createRandomUser(): User {
     let calender: CalenderDay[] = [];
@@ -19,13 +45,22 @@ export function createRandomUser(): User {
             carpoolingIntent: true,
             pickupPoint: {
                 address: String(faker.location.streetAddress()),
-                coordinates: [Number(faker.location.latitude()), Number(faker.location.latitude())]
+                coordinates: [
+                    Number(faker.location.latitude({
+                        min: centerLocation.coordinates[0] - d,
+                        max: centerLocation.coordinates[0] + d
+                    })),
+                    Number(faker.location.latitude({
+                        min: centerLocation.coordinates[1] - d,
+                        max: centerLocation.coordinates[1] + d
+                    }))]
             },
-            destination: {
-                address: String(faker.location.streetAddress()),
-                coordinates: [Number(faker.location.latitude()), Number(faker.location.latitude())]
-            },
-            timeOfArrival: String(faker.date.soon()),
+            destination: centerLocation,
+            timeOfArrival: (() => {
+                const hour: number = faker.number.int({ min: 0, max: hourTimes.length - 1 });
+                const minute: number = faker.number.int({ min: 0, max: minuteTimes.length - 1 });
+                return `${hourTimes[hour]}:${minuteTimes[minute]}`;
+            })()
         }
 
         calender.push(calenderDay);
