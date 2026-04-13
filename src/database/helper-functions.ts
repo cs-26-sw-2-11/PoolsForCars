96b01d5e018d6aad11bac7128f7f84418f495b72
jonat <jdamg25@student.aau.dk>
2026-04-13 12:13:12 +0200

    Now also has a asyncReadFileFunction, yes

--- src/database/helper-functions.ts
+++ src/database/helper-functions.ts
@@ -1,18 +1,24 @@
 import { promises as fsPromises } from 'fs';
 import {join} from 'path';
+import { json } from 'stream/consumers';
 
 async function asyncWriteFile(filename: string, data: any){
     try {
         await fsPromises.writeFile(join("src/database/", filename), data, {
             flag: 'w',
         });
+    } catch (err) {
+        console.log(err);
+        return 'Something went wrong';
+    }
+}
 
+async function asyncReadFile(filename: string){
+    try {
         const contents = await fsPromises.readFile(
             join("src/database/", filename),
             'utf-8',
         );
-        console.log(contents); 
-
         return contents;
     } catch (err) {
         console.log(err);
@@ -20,5 +26,8 @@ async function asyncWriteFile(filename: string, data: any){
     }
 }
 
+asyncWriteFile('data.json', JSON.stringify({name: 'John', age: 30}, null, 2));
 
-asyncWriteFile('data.json', JSON.stringify({ name: 'John', age: 30 }, null, 2));
+asyncReadFile('data.json').then((data) => {
+    console.log(JSON.parse(data));
+});
