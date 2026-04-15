497a6bc7460740a1952e457654a604b83a8cb999
aqzx2413 <aqzx2413@gmail.com>
2026-04-15 13:43:10 +0200

    idk

--- dist/app.d.ts
+++ dist/app.d.ts
@@ -1,5 +1,2 @@
-import { type User } from "./models/user.model.js";
-export declare const createUserFromForm: (user: User) => void;
 export declare const cleanFormString: (form: string) => "Something went wrong" | undefined;
-export declare const loginUser: (name: string, phoneNumber: string) => void;
 //# sourceMappingURL=app.d.ts.map
