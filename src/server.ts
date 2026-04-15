af9c8a9271aa549a7a77a0ca6ed8d5628990e437
aqzx2413 <aqzx2413@gmail.com>
2026-04-15 13:37:49 +0200

    functions to help server recieve form data from html site

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,22 @@ const port = 3410;
 
 // NEED TO HANDLE THE DIFFERENT REQUEST COMING IN FROM USER SIGNUP, LOGIN, GROUP CREATION AND SUCH
 
+
+// Responsible for processing form request to a string, it recieves the incoming message
+export const handleRequest = async (req: http.IncomingMessage): Promise<string> => {
+        // Stores the data as a buffer object, which contains binary data
+        const chunks: Buffer[] = [];
+
+        // When a request comes, it pushes data to the buffer array
+        for await (const chunk of req) {
+            chunks.push(chunk as Buffer);
+        }
+
+        // Once all the data has arrived, it removes the spaces in the buffer
+        // and converts the binary data into a string.
+        return Buffer.concat(chunks).toString()
+}
+
 export const fileResponse = async (res: http.ServerResponse, filename: string) => {
     const sPath = filename;
     console.log("Reading:" + sPath);
