c0603573a03d630b348268deb522647f87850959
Jeppe Vad Andersen <Jeppe.vadandersen@gmail.com>
2026-04-16 13:12:07 +0200

    Update: Integrating meta into each model

---
+++ dist/models/week.model.d.ts
@@ -0,0 +1,7 @@
+import { type CalenderDay } from "./calender_day.model.js";
+export interface Week {
+    weekNumber: number;
+    dateSpan: string;
+    days: [CalenderDay, CalenderDay, CalenderDay, CalenderDay, CalenderDay];
+}
+//# sourceMappingURL=week.model.d.ts.map
