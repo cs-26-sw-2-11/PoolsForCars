ec416c31f187fdedd0a486923f94c3b46abfcfae
aqzx2413 <aqzx2413@gmail.com>
2026-04-20 11:46:41 +0200

    The server now handles login requests

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,26 @@
 import express from "express";
 import routes from "./routes/index.js";
+import * as path from 'path';
 
-//Defines the used variables
-const hostname = '127.0.0.1';
+//=== VARIABLES ===//
+
+// Our port number on the IWP server
 const port = 3410;
+
+const hostname = '127.0.0.1';
 const app = express();
+const filePath = path.resolve(process.cwd());
 
 
-
-//Express logic to ...
+//=== SOMETHING ===//
+// explain every lines
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/ScriptsForUsers", express.static(filePath+"/dist/html_scripts"));
+app.use("/CSSforHTML", express.static(filePath+"/src/PublicResources"));
 app.use(routes);
 
-//Listens for the server on a specific port
+
+//=== STARTS SERVER ===///
 app.listen(port)
 console.log(`Server running at http://${hostname}:${port}/`);
