12c0d3512999386a74fa047e6b5ff84294ac3187
Jeppe Vad Andersen <Jeppe.vadandersen@gmail.com>
2026-04-21 11:51:02 +0200

    Adding routing function to openroute service part. Coordinates are swapped for some reason :shrug:

--- src/openrouteservice.ts
+++ src/openrouteservice.ts
@@ -6,33 +6,41 @@ dotenv.config();
 
 
 const client = new OpenRouteService({
-   apiKey: process.env.ORS_API_KEY || "",
+    apiKey: process.env.ORS_API_KEY || "",
 });
 
 async function geocodingExamples() {
-   try {
-      // Search for a place
-      const searchResults = await client.geocoding.search({
-         // text: "Fibigerstræde 15, 9220 Aalborg",
-         text: "pizza near me",
-         // size: 5,
-         // layers: ["address", "country"],
-         // "focus.point": [57.012186, 9.992092],
-         // "boundary.circle": [57.012186, 9.992092, 1],
-         "focus.point": [9.992092, 57.012186],
-         "boundary.circle": [9.992092, 57.012186, 1],
-         "boundary.country": ["DK"]
-      });
+    try {
+        // Search for a place
+        const searchResults = await client.geocoding.search({
+            // text: "Fibigerstræde 15, 9220 Aalborg",
+            text: "pizza near me",
+            // size: 5,
+            // layers: ["address", "country"],
+            // "focus.point": [57.012186, 9.992092],
+            // "boundary.circle": [57.012186, 9.992092, 1],
+            "focus.point": [9.992092, 57.012186],
+            "boundary.circle": [9.992092, 57.012186, 1],
+            "boundary.country": ["DK"]
+        });
 
-      console.log("Search results:", searchResults.features.length);
-      searchResults.features.forEach((feature, index)=> {
-        console.log(`Result: ${index}`, feature.properties);
-        console.log(`Result: ${index}`, feature.geometry);
-      });
+        console.log("Search results:", searchResults.features.length);
+        searchResults.features.forEach((feature, index) => {
+            console.log(`Result: ${index}`, feature.properties);
+            console.log(`Result: ${index}`, feature.geometry);
+        });
 
-   } catch (error) {
-      console.error("Error:", error);
-   }
+    } catch (error) {
+        console.error("Error:", error);
+    }
 }
 
-geocodingExamples();
+// Get a route
+export const getRoute = async (coordinate1: [number, number], coordinate2: [number, number]) => {
+    return await client.directions.calculateRoute("driving-car", {
+        coordinates: [
+            [coordinate1[1], coordinate1[0]],
+            [coordinate2[1], coordinate2[0]],
+        ],
+    })
+}
