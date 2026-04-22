3ce53831a0c489944eb29231a5d1f2468ccfbc31
Jeppe Vad Andersen <Jeppe.vadandersen@gmail.com>
2026-04-22 11:42:45 +0200

    Working on adding calender routes and making the controller

---
+++ src/controllers/calender.controller.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+import * as calenderModel from '../models/calender_day.model.js';
+
+
+// ───────────────────────────────────────────────────────────────
+//  :::::: MAIN CRUD FUNCTIONS ::::::
+// ───────────────────────────────────────────────────────────────
+
+// export const getCalender = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+//     const fakeUser: userModel.User = await userSeeder.createRandomUser();
+//     res.status(200).json(fakeUser);
+// }
+
+// export const createWeek = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+//
+// }
---
+++ src/routes/calenderRoutes.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+export const calenderRoutes = express.Router();
+
+import * as calenderController from '../controllers/calender.controller.js';
+
+
+// calenderRoutes.get('/', calenderController.getCalenders);
+//
+// calenderRoutes.get('/:calenderId', calenderController.getcalenderById);
+//
+// calenderRoutes.post('/', calenderController.createcalender);
+
