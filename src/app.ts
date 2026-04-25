988336b7bac5290321c3691388945fb25fe8f19d
Jeppe Vad Andersen <Jeppe.vadandersen@gmail.com>
2026-04-25 18:05:43 +0200

    Clearing database for testing every time the program starts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import { initUsers } from "./models/user.model.js";
+import { clearUsers, initUsers } from "./models/user.model.js";
 import * as server from "./server.js";
 
+await clearUsers();
 await initUsers();
 
 //We use EC6 modules
