7d14c1ceea2f8f2315e286e2df332df4723675a5
Jeppe Vad Andersen <Jeppe.vadandersen@gmail.com>
2026-04-25 23:46:14 +0200

    Added clearing (for testing) and loading of groups as initialization steps

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import { clearUsers, initUsers } from "./models/user.model.js";
+import { clearGroups, initGroups } from "./models/group.model.js";
 import * as server from "./server.js";
 
 await clearUsers();
+await clearGroups();
 await initUsers();
+await initGroups();
 
 //We use EC6 modules
 
