4710a2615f4448f1fa9a4e65ef1919eec865f934
Jeppe Vad Andersen <Jeppe.vadandersen@gmail.com>
2026-04-25 18:14:49 +0200

    Functions to get a users entire calender or a sinlge week by number

---
+++ src/controllers/calendar.controller.ts
@@ -0,0 +1,41 @@
+import express from "express";
+
+import * as userModel from '../models/user.model.js';
+import * as weekModel from "../models/week.model.js";
+import * as calendarModel from "../models/calendar.model.js";
+
+// ───────────────────────────────────────────────────────────────
+//  :::::: MAIN CRUD FUNCTIONS ::::::
+// ───────────────────────────────────────────────────────────────
+
+export const getCalendar = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const user: userModel.User = await userModel.readUser(Number(req.params['userId']));
+    const calendar: calendarModel.Calendar = calendarModel.readCalendar(user);
+    res.status(200).json(calendar);
+}
+
+export const getCalendarWeek = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const user: userModel.User = await userModel.readUser(Number(req.params['userId']));
+        const calendar: calendarModel.Calendar = calendarModel.readCalendar(user)
+
+        const weekNumber: number = Number(req.params['weekNumber']);
+        let week: weekModel.Week = await calendarModel.readWeek(calendar, weekNumber);
+
+        if (typeof week === 'undefined') {
+            week = await calendarModel.createWeek(calendar, weekNumber, user.schedule);
+            userModel.updateUser(user.id, user);
+        }
+
+        console.log(week);
+
+        res.status(200).json(week);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+// export const createWeek = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+//
+// }
