e8190c37105edf0890c6a6673f68a5c4238b4b39
Jeppe Vad Andersen <Jeppe.vadandersen@gmail.com>
2026-04-25 18:07:30 +0200

    Setting up group routes

---
+++ src/routes/groupRoutes.ts
@@ -0,0 +1,13 @@
+//=== IMPORTS ===///
+import express from 'express';
+import { filePath } from "./index.js"
+// import { searchForGroups } from '../matching/group_finder.js';
+
+const router = express.Router();
+
+
+// router.route("/:userId")
+//     .post(searchForGroups);
+
+
+export default router;
