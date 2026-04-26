9820a8f17cdeb353a62ea515a99fcee6ef6d83d8
Jeppe Vad Andersen <Jeppe.vadandersen@gmail.com>
2026-04-26 19:51:23 +0200

    Loop over all days in all week and cal searchForGroups on a given day

--- src/controllers/group.controller.ts
+++ src/controllers/group.controller.ts
@@ -2,7 +2,8 @@ import express from "express";
 
 import * as userModel from '../models/user.model.js';
 import * as groupService from "../services/group.service.js";
-import { groupEnd } from "node:console";
+import * as compatibilityModel from "../models/compatibility.model.js";
+import { findEligbleDrivers } from "../matching/temporal_compatibility.js";
 
 
 // ───────────────────────────────────────────────────────────────
@@ -11,8 +12,19 @@ import { groupEnd } from "node:console";
 
 export const searchForGroups = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const user: userModel.User = await userModel.readUser(Number(req.params['userId']));
+    const compatibilityMap: compatibilityModel.WeeklyCompatibilityIndex = await findEligbleDrivers(user);
 
-    res.status(200).json();
+    console.log(`Searching for groups for user ${user.id}`);
+    console.log(JSON.stringify(compatibilityMap, null, 2));
+
+    for (const week of Object.entries(user.calendar)) {
+        for (const day of Object.entries(week[1].days)) {
+            if (day[1].carAvailability) continue; // only passengers search for groups
+            await groupService.searchForGroups(user.id, day[1], compatibilityMap, Number(week[0]), day[0]);
+        }
+
+    }
+    res.status(200).json(user);
 }
 
 
