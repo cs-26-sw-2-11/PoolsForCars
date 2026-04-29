2bd4772198ab8362edce9112dc7245de098e55cf
Jeppe Vad Andersen <Jeppe.vadandersen@gmail.com>
2026-04-29 14:54:57 +0200

    Moved week and day loops into group finder file

--- src/controllers/group.controller.ts
+++ src/controllers/group.controller.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 import * as userModel from '../models/user.model.js';
-import * as groupService from "../services/group.service.js";
+import * as groupService from "../services/groups/group.service.js";
 import * as compatibilityModel from "../models/compatibility.model.js";
 import { findEligbleDrivers } from "../matching/temporal_compatibility.js";
 
@@ -14,21 +14,20 @@ export const searchForGroups = async (req: express.Request, res: express.Respons
     const user: userModel.User = await userModel.readUser(Number(req.params['userId']));
     const compatibilityMap: compatibilityModel.WeeklyCompatibilityIndex = await findEligbleDrivers(user);
 
+    console.log("\n");
+    console.log("\n");
+    console.log("\n");
     console.log(`Searching for groups for user ${user.id}`);
     console.log(JSON.stringify(compatibilityMap, null, 2));
 
-    for (const week of Object.entries(user.calendar)) {
-        for (const day of Object.entries(week[1].days)) {
-            if (day[1].carAvailability) continue; // only passengers search for groups
-            await groupService.searchForGroups(user.id, day[1], compatibilityMap, Number(week[0]), day[0]);
-        }
+    groupService.searchForGroups(user, compatibilityMap);
 
-    }
     res.status(200).json(user);
 }
 
 
 export const makeAllGroups = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log("alloo");
     const user: userModel.User = await userModel.readUser(Number(req.params['userId']));
     for (const week of Object.values(user.calendar)) {
         for (const day of Object.values(week.days)) {
