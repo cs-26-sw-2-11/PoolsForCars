d9637ebf56aabff080085bc538aaaba9738ee0eb
LyingRat <lyra_w.t@outlook.com>
2026-05-06 18:13:41 +0200

    Group names and availability now change depending on mode

--- src/PublicResources/JS/groups.js
+++ src/PublicResources/JS/groups.js
@@ -1,23 +1,62 @@
+//Subjects to be changed
+
+let currentDay = "Monday";
+let currentMode = "driver";
+
+// Groups and stuff that decides when they show up
+
+const groups = [
+  {day: "Monday", role: "driver", name: "ALG med Gutterne"},
+  {day: "Tuesday", role: "passenger", name: "Skyd mig det er en SLIAL dag"},
+  {day: "Wednesday", role: "passenger", name: "DEB er BED baglæns"},
+  {day: "Friday", role: "driver", name: "IWP står for I Wanna Pray"}
+];
+
+function renderGroups() {
+  
+  const groupTitleElement = document.querySelector(".group-title");
+  
+  const visibleGroups = groups.filter(group => {
+  return (
+    group.day === currentDay &&
+    group.role === currentMode
+  );
+});
+
+const activeGroup = visibleGroups[0];
+
+if (!activeGroup) {
+  groupTitleElement.textContent = "No group available";
+  return;
+}
+
+groupTitleElement.textContent = activeGroup.name;
+
+
+}
+
 // Things to do with modes
 
 function setMode(mode) {
   const allowedModes = ["driver","passenger"];
   if (!allowedModes.includes(mode)) {
-    console.warn("Invalid mode detected");
+    console.warn("Invalid mode detected!");
     return;
   }
+  currentMode = mode;
 
 /// Changing of the title
 
   document.body.classList.remove("mode-driver", "mode-passenger");
   document.body.classList.add(`mode-${mode}`);
 
-  const title = document.getElementById("page-title");
+  const title = document.getElementById("pageTitle");
   if (mode === "driver") {
     title.textContent = "My Groups - Driver";
   } else {
     title.textContent = "My Groups - Passenger";
   }
+  renderGroups();
 }
 
 /// Beeg toggle
@@ -28,7 +67,7 @@ modeToggle.addEventListener("change", () => {
   let mode;
 
   if (modeToggle.checked) {
-     mode = "passenger";
+    mode = "passenger";
   } else {
     mode = "driver";
   }
@@ -46,6 +85,8 @@ if (modeToggle.checked) {
 setMode(initialMode);
 
 
+
+
 // Grab all weekday items
 const dayItems = document.querySelectorAll(".day-list li");
 
@@ -57,6 +98,7 @@ const schedules = {
   Tuesday: [
     { time: "07:00", address: "Vestergade 12" },
     { time: "07:20", address: "Kattevej 21" }
+
   ],
   Wednesday: [],
   Thursday: [],
@@ -100,7 +142,12 @@ dayItems.forEach(day => {
     day.classList.add("active");
 
     const selectedDay = day.textContent;
+    
+    currentDay = selectedDay;
+    
     renderSchedule(selectedDay);
+
+    renderGroups();
   });
 });
 
