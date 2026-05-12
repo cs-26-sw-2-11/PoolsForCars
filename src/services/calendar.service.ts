c908a58b9bb05bf0c3f395e91b212bf2c18c79ba
Jeppe Vad Andersen <Jeppe.vadandersen@gmail.com>
2026-05-12 15:14:37 +0200

    Migrating functionality out of calendar controller. Begin work on handling calendar updates.

---
+++ src/services/calendar.service.ts
@@ -0,0 +1,13 @@
+import * as userModel from "../models/user.model.js"
+import * as calendarModel from "../models/calendar.model.js"
+
+
+
+
+export const identifyCalendarDiffs = (c1: calendarModel.Calendar, c2: calendarModel.Calendar) => {
+
+}
+
+export const cleanCalendar = (user: userModel.User) => {
+
+}
