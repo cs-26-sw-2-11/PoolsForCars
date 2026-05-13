434bf9703d535cc1fb60d72f67b7bc9a10e1ac57
Jeppe Vad Andersen <Jeppe.vadandersen@gmail.com>
2026-05-13 23:19:18 +0200

    testing

--- src/services/local_ORS_test.ts
+++ src/services/local_ORS_test.ts
@@ -10,39 +10,39 @@ function isDirectionsResponse(data: DirectionsResponse): data is DirectionsRespo
 }
 
 const getRoute = async (coordinate1: [number, number], coordinate2: [number, number]) => {
-        const apiUrl: string = 'http://172.25.11.233:8080/v2/directions/driving-car/json';
-        try {
-            const response: Response = await fetch(apiUrl, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    // 'Authorization': apiKey,
-                },
-                body: JSON.stringify(
-                    {
-                        "coordinates": [
-                            [coordinate1[1], coordinate1[0]],
-                            [coordinate2[1], coordinate2[0]],
-                        ]
-                    }
-                ),
-            });
+    const apiUrl: string = 'http://172.25.11.233:8080/v2/directions/driving-car/json';
+    try {
+        const response: Response = await fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                // 'Authorization': apiKey,
+            },
+            body: JSON.stringify(
+                {
+                    "coordinates": [
+                        [coordinate1[1], coordinate1[0]],
+                        [coordinate2[1], coordinate2[0]],
+                    ]
+                }
+            ),
+        });
 
-            if (!response.ok) {
-                throw new Error(`OpenRouteService error getting route. Status: ${response.status}`);
-            }
-
-            const data: DirectionsResponse = await response.json();
-
-            if (isDirectionsResponse(data)) {
-                return data;
-            } else {
-                throw new Error("Received data does not match DirectionsResponse format");
-            }
-        } catch (error) {
-            console.error("Failed to fetch route:", error);
-            throw error;
+        if (!response.ok) {
+            throw new Error(`OpenRouteService error getting route. Status: ${response.status}`);
         }
+
+        const data: DirectionsResponse = await response.json();
+
+        if (isDirectionsResponse(data)) {
+            return data;
+        } else {
+            throw new Error("Received data does not match DirectionsResponse format");
+        }
+    } catch (error) {
+        console.error("Failed to fetch route:", error);
+        throw error;
+    }
 }
 
 const route: DirectionsResponse = await getRoute(
