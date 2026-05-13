fea7e9db1032299227fb8899be4c3d2c33fcf551
Jeppe Vad Andersen <Jeppe.vadandersen@gmail.com>
2026-05-13 13:58:26 +0200

    No more clearing groups on startup.

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,10 @@ import { clearGroups, initGroups } from "./models/group.model.js";
 import * as server from "./server.js";
 import { clearNotifications, initNotifications } from "./models/notification.model.js";
 
-await clearGroups();
+// await clearGroups();
 await initUsers();
 await initGroups();
-await initNotifications();
+// await initNotifications();
 
 //We use EC6 modules
 
